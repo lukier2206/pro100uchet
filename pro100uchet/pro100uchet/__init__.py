cf54dc2ca04e714382b91a1baf4ed2a980204c94
lukier2206 <60492139+lukier2206@users.noreply.github.com>
2021-12-15 19:13:53 +0100

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=ca3bca0dfb42903df7bf&state=c2b71c98-7f5d-42c3-b4b8-0083cb1e9de6

---
+++ pro100uchet/pro100uchet/__init__.py
@@ -0,0 +1,2 @@
+vscode://vscode.github-authentication/did-authenticate?windowid=1&code=ca3bca0dfb42903df7bf&state=c2b71c98-7f5d-42c3-b4b8-0083cb1e9de6
+vscode://vscode.github-authentication/did-authenticate?windowid=1&code=ca3bca0dfb42903df7bf&state=c2b71c98-7f5d-42c3-b4b8-0083cb1e9de6
